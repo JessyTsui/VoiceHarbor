96ad88c5db7b0d9dc5392da151f4f757ab7ee4fa
JessyTsu1 <51992423+JessyTsu1@users.noreply.github.com>
2024-03-12 19:15:02 +0800

    Update run_api.sh

--- run_api.sh
+++ run_api.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
+# export CUDA_VISIBLE_DEVICES=0
 
 python app.py \
     --model-name openai/whisper-large-v3 \
     --batch-size 24 \
-    --port 9000
+    --port 9000\
+    --device-id mps
